fc1c5a55205e2dc6b3e30bec00af58d04bfb779b
groovey <pokoot@gmail.com>
2020-10-17 19:27:57 +0800

    Fix error template requires one element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        HelloWorld,
+    }
+};
 </script>
 
 <style>
