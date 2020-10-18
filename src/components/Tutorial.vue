591cdc5d66631654bb925a089ed2499f585179c8
groovey <pokoot@gmail.com>
2020-10-19 03:04:25 +0800

    Learning

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -1,108 +1,113 @@
 <template>
   <div>
     <div>
-      <div class="container">
-        <h3>V-model for input</h3>
-        <div class="left">
-          <form @submit.prevent="add">
-            <input type="text" placeholder="first name" name="firstName" v-model="firstName" /> <br />
-            <input type="text" placeholder="last name" name="lastName" v-model="lastName" /> <br />
-            <input type="text" placeholder="email" name="email" v-model="email" /> <br />
-            <input type="submit" value="submit" />
-          </form>
-        </div>
-        <div class="right">
-          <div>{{ users }}</div>
-          <div>Computed fullName : <input v-model="fullName" /></div>
-        </div>
+      <div>Props v-bind id value = {{ id }}</div>
+      <div>Props title value as string = {{ title }}</div>
+      <div>Props title value with html code = <span v-html="html"></span></div>
+      <div>The v bind image directive = <img v-bind:src="img" width="20" /></div>
+      <div v-bind:class="class_placeholder">The v bind class via placeholder</div>
+      <div v-bind:class="[class_one, class_two]">The v bind class via array placeholder</div>
+      <div v-bind:class="{ white_font: true, blue_background: true }">The v bind class via json object</div>
+      <div v-bind:class="better_placeholder">The v bind class via json object (A better way of doing it)</div>
+      <div :class="highlight">The v bind class via computed function</div>
+      <div>
+        <input v-on:click="send" type="button" value="Emit Data" />
       </div>
     </div>
 
-    <div>
-      <div class="container">
-        <h3>V-model for textarea</h3>
-        <div class="left">
-          <textarea rows="5" v-model="message.textarea"></textarea>
-        </div>
-        <div class="right">
-          {{ message.textarea }}
-        </div>
+    <div class="container">
+      <h3>V-model for input</h3>
+      <div class="left">
+        <form @submit.prevent="add">
+          <input type="text" placeholder="first name" name="firstName" v-model="firstName" /> <br />
+          <input type="text" placeholder="last name" name="lastName" v-model="lastName" /> <br />
+          <input type="text" placeholder="email" name="email" v-model="email" /> <br />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+      <div class="right">
+        <div>{{ users }}</div>
+        <div>Computed fullName : <input v-model="fullName" /></div>
       </div>
     </div>
 
-    <div>
-      <div class="container">
-        <h3>V-model for radio</h3>
-        <div class="left">
-          <div>
-            <label for="radio_apple">Apple</label>
-            <input id="radio_apple" type="radio" value="apple" v-model="message.radio" />
-          </div>
-          <div>
-            <label for="radio_banana">Banana</label>
-            <input id="radio_banana" type="radio" value="banana" v-model="message.radio" />
-          </div>
-          <div>
-            <label for="radio_orange">Orange</label>
-            <input id="radio_orange" type="radio" value="orange" v-model="message.radio" />
-          </div>
-        </div>
-        <div class="right">
-          {{ message.radio }}
-        </div>
+    <div class="container">
+      <h3>V-model for textarea</h3>
+      <div class="left">
+        <textarea rows="5" v-model="message.textarea"></textarea>
+      </div>
+      <div class="right">
+        {{ message.textarea }}
       </div>
     </div>
 
-    <div>
-      <div class="container">
-        <h3>V-model for checkbox</h3>
-        <div class="left">
-          <div>
-            <label for="checkbox_apple">Apple</label>
-            <input id="checkbox_apple" type="checkbox" value="apple" v-model="message.checkbox" />
-          </div>
-          <div>
-            <label for="checkbox_banana">Banana</label>
-            <input id="checkbox_banana" type="checkbox" value="banana" v-model="message.checkbox" />
-          </div>
-          <div>
-            <label for="checkbox_orange">Orange</label>
-            <input id="checkbox_orange" type="checkbox" value="orange" v-model="message.checkbox" />
-          </div>
+    <div class="container">
+      <h3>V-model for radio</h3>
+      <div class="left">
+        <div>
+          <label for="radio_apple">Apple</label>
+          <input id="radio_apple" type="radio" value="apple" v-model="message.radio" />
         </div>
-        <div class="right">
-          {{ message.checkbox }}
+        <div>
+          <label for="radio_banana">Banana</label>
+          <input id="radio_banana" type="radio" value="banana" v-model="message.radio" />
         </div>
+        <div>
+          <label for="radio_orange">Orange</label>
+          <input id="radio_orange" type="radio" value="orange" v-model="message.radio" />
+        </div>
+      </div>
+      <div class="right">
+        {{ message.radio }}
       </div>
     </div>
 
-    <div>
-      <div class="container">
-        <h3>V-model for select</h3>
-        <div class="left">
-          <select name="" id="" v-model="message.select">
-            <option value="apple">Apple</option>
-            <option value="banana">Banana</option>
-            <option value="orange">Orange</option>
-          </select>
+    <div class="container">
+      <h3>V-model for checkbox</h3>
+      <div class="left">
+        <div>
+          <label for="checkbox_apple">Apple</label>
+          <input id="checkbox_apple" type="checkbox" value="apple" v-model="message.checkbox" />
         </div>
-        <div class="right">
-          {{ message.select }}
+        <div>
+          <label for="checkbox_banana">Banana</label>
+          <input id="checkbox_banana" type="checkbox" value="banana" v-model="message.checkbox" />
         </div>
+        <div>
+          <label for="checkbox_orange">Orange</label>
+          <input id="checkbox_orange" type="checkbox" value="orange" v-model="message.checkbox" />
+        </div>
+      </div>
+      <div class="right">
+        {{ message.checkbox }}
       </div>
     </div>
 
-    <div>
-      <div class="container">
-        <h3>Dynamic select base on model data</h3>
-        <div class="left">
-          <select name="" id="" v-model="message.fruit">
-            <option v-for="fruit in fruits" :key="fruit" :value="fruit">{{ fruit }}</option>
-          </select>
-        </div>
-        <div class="right">
-          {{ message.fruit }}
-        </div>
+    <div class="container">
+      <h3>V-model for select</h3>
+      <div class="left">
+        <select name="" id="" v-model="message.select">
+          <option value="">Select fruit</option>
+          <option value="apple">Apple</option>
+          <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
+        </select>
+      </div>
+      <div class="right">
+        {{ message.select }}
+      </div>
+    </div>
+
+    <div class="container">
+      <h3>Dynamic select base on model data</h3>
+      <div class="left">
+        <select name="" id="" v-model="message.fruit">
+          <option value="">Select fruit</option>
+          <option v-for="fruit in fruits" :key="fruit" :value="fruit">{{ fruit }}</option>
+        </select>
+      </div>
+      <div class="right">
+        {{ message.fruit }}
       </div>
     </div>
   </div>
@@ -110,7 +115,10 @@
 
 <script>
 export default {
-
+  props: {
+    title: String,
+    id: Number
+  },
   data () {
     return {
       message: {
@@ -123,10 +131,21 @@ export default {
       fruits: [
         'apple', 'orange', 'banana'
       ],
+      description: 'This is a description from the component.',
+      fullDescription: 'This the full description.',
       firstName: '',
       lastName: '',
       email: '',
-      users: []
+      users: [],
+      html: '<b><u>' + this.title + '</u></b>',
+      img: 'http://localhost:8080/img/logo.82b9c7a5.png',
+      class_placeholder: 'red_font',
+      class_one: 'white_font',
+      class_two: 'orange_background',
+      better_placeholder: {
+        white_font: true,
+        green_background: true
+      }
     }
   },
   methods: {
@@ -135,6 +154,10 @@ export default {
       this.firstName = ''
       this.lastName = ''
       this.email = ''
+    },
+    send () {
+      this.$emit('relay', this.description, this.fullDescription)
+      console.log(this.description)
     }
   },
   computed: {
@@ -147,6 +170,12 @@ export default {
         const array = value.split(' ')
         this.firstName = array[0]
         this.lastName = array[array.length - 1]
+      }
+    },
+    highlight () {
+      return {
+        green_font: true,
+        yellow_background: true
       }
     }
   }
@@ -168,5 +197,32 @@ export default {
 
 .right {
   width: 50%;
+}
+
+.white_font {
+  color: white;
+}
+.red_font {
+  color: red;
+}
+
+.green_font {
+  color: green;
+}
+
+.blue_background {
+  background-color: blue;
+}
+
+.orange_background {
+  background-color: orange;
+}
+
+.green_background {
+  background-color: green;
+}
+
+.yellow_background {
+  background-color: yellow;
 }
 </style>
--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <div>The url parameter test/:id value = {{ id }}</div>
-    <div>Value contains html code = <span v-html="html"></span></div>
-    <div>The v bind image directive = <img v-bind:src="img" width="20" /></div>
+    <div>Tutorial description = {{ description }}</div>
 
     <User header="Listing of users"></User>
-    <Tutorial accountBalance="kim"></Tutorial>
+    <Tutorial title="This is a vue tutorial" :id="id" v-on:relay="processRelay"></Tutorial>
   </div>
 </template>
 
@@ -21,8 +20,15 @@ export default {
   data () {
     return {
       id: this.$route.params.id,
-      html: '<b><u>Contains html code</u></b>',
-      img: 'http://localhost:8080/img/logo.82b9c7a5.png'
+      description: 'Welcome to the tutorial page.'
+
+    }
+  },
+  methods: {
+    processRelay (description, fullDescription) {
+      console.log('description = ', description)
+      console.log('full description = ', fullDescription)
+      this.description = description
     }
   }
 
@@ -31,7 +37,8 @@ export default {
 
 <style scoped>
 div {
-  padding: 10px;
+  padding: 5px;
   background-color: azure;
+  margin-bottom: 5px;
 }
 </style>
