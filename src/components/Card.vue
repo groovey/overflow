a40d20638a622ab0cf04a8ec1adcc96fc9b617fc
groovey <pokoot@gmail.com>
2020-10-25 17:26:00 +0800

    Added card

---
+++ src/components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bg-white rounded-sm shadow">
+    <span v-if="hasHeader" :class="renderHeader">
+      <slot name="header"></slot>
+    </span>
+
+    <div class="px-4 pb-4 pt-3">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    color: {
+      type: String,
+      required: false
+    },
+    custom: String,
+    border: Boolean
+
+  },
+
+  setup (props, context) {
+    const tw = {
+      header: 'px-4 py-3 block font-bold text-sm uppercase tracking-widest text-primary',
+      border: 'border-b border-gray-100',
+      custom: props.custom
+    }
+
+    const render = computed(() => {
+      const ui = [tw.base]
+
+      ui.push(tw.base)
+
+      return ui.join(' ')
+    })
+
+    const hasHeader = computed(() => {
+      return !!context.slots.header
+    })
+
+    const renderHeader = computed(() => {
+      var ui = []
+      if (props.border) ui.push(tw.border)
+      ui.push(tw.header)
+
+      return ui.join(' ')
+    })
+
+    return { render, hasHeader, renderHeader }
+  }
+
+}
+</script>
+
+<style scoped lang="postcss">
+</style>
--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
+  <div class="p-4">
     <div class="bg-green-100">Welcome to testing</div>
+
+    <!-- Button -->
     <div class="w-64 bg-green-300">
       <x-button @click.prevent="test" color="primary" small>Sample</x-button>
     </div>
+
+    <!-- Bullet -->
     <div><x-bullet color="success"></x-bullet>Sample</div>
+
+    <!-- Alert -->
     <x-alert color="error">
-      <template v-slot:header>MY HEADER</template>
+      <template v-slot:header border>ALERT HEADER</template>
       This is the content of the alert
     </x-alert>
+
+    <!-- Card -->
+    <x-card border>
+      <template v-slot:header>CARD HEADER</template>
+      1234
+    </x-card>
   </div>
 </template>
 
@@ -16,11 +28,13 @@
 import XButton from '@/components/Button.vue'
 import XBullet from '@/components/Bullet.vue'
 import XAlert from '@/components/Alert.vue'
+import XCard from '@/components/Card.vue'
 export default {
   components: {
     XButton,
     XBullet,
-    XAlert
+    XAlert,
+    XCard
   },
   data () {
     return {}
@@ -34,5 +48,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
 </style>
